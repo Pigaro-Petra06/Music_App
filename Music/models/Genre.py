8dd68d0bb364d07bf4811a8d276506ee3a72700a
Pigaro-Petra06 <76613144+Pigaro-Petra06@users.noreply.github.com>
2021-01-07 00:47:47 +0800

    Delete Genre.py

--- Music/models/Genre.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    class Meta:
-        app_label = 'Music'
-
-    def __str__(self):
-        return f'{self.name}'
